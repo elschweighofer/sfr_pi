8b960c31dbbe1de91a6ea4de370d6c9aa3de11a5
Pascal <pascal.sokal@gmail.com>
2023-03-10 20:26:51 +0100

    split script and made them executable

--- kafka/dev/producer.sh
+++ kafka/dev/producer.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-docker-compose up -d
 docker exec --interactive --tty broker \
-kafka-topics --create --topic sensor_readings --bootstrap-server broker:9092
 kafka-console-producer \
   --topic sensor_readings \
   --bootstrap-server broker:9092 \
