2bab3f496e59308f74447205eda447e6d8f740d2
Lukas Schweighofer <lukas.schweighofer@outlook.at>
2023-03-12 13:41:41 +0100

    changes to gauge and css

--- nodejs/public/js/sensordata.js
+++ nodejs/public/js/sensordata.js
@@ -1,33 +1,42 @@
 const socket = io();
 var temperatureGauge;
 var humidityGauge;
+
+
+
 //on message 
 socket.on('message', function (msg) {
     jsonMsg = JSON.parse(msg);
     var temperature = jsonMsg.temperature;
     var humidity = jsonMsg.humidity;
+    var host = jsonMsg.host;
     temperatureGauge.value=temperature;
     humidityGauge.value=humidity;
 });
 
+
+
 function initGauge() {
     temperatureGauge = new LinearGauge({
-        renderTo: 'temperature-gauge',
-        width: 150,
-        height: 400,
-        units: '°C',
-        title: "Temperature",
-        value: 26,
-        minValue: 0,
-        maxValue: 100,
-        highlights: [
-            {
-                "from": 30,
-                "to": 100,
-                "color": "rgba(200, 50, 50, .75)"
-            }
-        ],
+      renderTo: 'temperature-gauge',
+      width: 150,
+      height: 400,
+      units: '°C',
+      title: 'Temperature',
+      value: 23,
+      minValue: -20,
+      maxValue: 100,
+      valueBox: false,
+      majorTicks: [-20,-10,0,10,20,30,40,50,60,70,80,90,100],
+      highlights: [
+        {
+          from: 30,
+          to: 100,
+          color: 'rgba(200, 50, 50, .75)',
+        },
+      ],
     }).draw();
+  
 
     humidityGauge = new RadialGauge({
         renderTo: 'humidity-gauge',
